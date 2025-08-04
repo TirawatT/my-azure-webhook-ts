f68def1d233742b9b6383e3175620245e68bc712
tirawatt <tirawat.thongsakun@gmail.com>
2025-08-04 17:35:55 +0700

    fix: change path prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,6 @@
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { PrismaClient } from "../app/generated/prisma";
+// import { PrismaClient } from "../app/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
