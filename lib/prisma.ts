84c5652e52f98dac07bbfb432226b252672af890
tirawatt <tirawat.thongsakun@gmail.com>
2025-08-04 16:39:07 +0700

    fix: build ignoreDuringBuilds

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -11,3 +11,20 @@ const prisma =
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
+
+// import { PrismaClient } from "@prisma/client";
+
+// let prisma: PrismaClient;
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient;
+// };
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!globalForPrisma.prisma) {
+//     globalForPrisma.prisma = new PrismaClient();
+//   }
+//   prisma = globalForPrisma.prisma;
+// }
+
+// export default prisma;
